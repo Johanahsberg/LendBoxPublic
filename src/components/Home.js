ed4fc7ce82538c6f1c77cd6899b6c204ac63af65
MusaAndrea <sjg14amu@student.lu.se>
2021-12-13 10:07:47 +0100

    Changed UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,17 @@ import wave from '../images/wave.svg'
 
 const Home = (props) => {
     return (
+        <>
         <div className="contentHome">
         <h1>Welcome to LendBox</h1> 
          <p>The free way to try board games</p>
          <p id = "textHome">Sign up now to rent Board Games free of charge for 14 days</p>
          <button onClick= {()=> window.location.href = "/LendBoxPublic/#/Register"} id="signUpHomeBtn" type="button" className="btn btn-info btn-lg">Sign up</button>
-         <video src={video1} controls="controls" />
-
         </div>
+         <div className = "videoDiv">
+             <video src={video1} controls="controls" />
+         </div>
+        </>
     )
     
 }
